0fb3bbbcb0f4c3a7b5e4249d800a97b622d11112
Donald Wasserman <djwasserman@gmail.com>
2017-06-18 09:43:29 -0400

    bunch of code changes to fix problem on my computer

--- functions/bills/src/mi.js
+++ functions/bills/src/mi.js
@@ -161,7 +161,6 @@ module.exports = {
               model.status = sf[action];
             }
           });
-          console.log(model.export());
           resolve(model.export());
         })
         .catch(err => {
