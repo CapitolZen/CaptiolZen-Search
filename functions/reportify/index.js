993fdcaafa45e61c3634a704395add72d4cac8d9
Donald Wasserman <djwasserman@gmail.com>
2017-07-22 16:24:44 -0400

    update output file

--- functions/reportify/index.js
+++ functions/reportify/index.js
@@ -3,13 +3,8 @@ const main = require('./main');
 exports.handle = function (event, context, callback) {
 
   main(event)
-    .then(output => {
-      let data = {
-        data: output[0],
-        nextBill: (output[1].exists) ? output[1].id : false
-      };
-
-      callback(null, data);
+    .then(url => {
+      callback(null, {url: url});
 
     })
     .catch(err => {
